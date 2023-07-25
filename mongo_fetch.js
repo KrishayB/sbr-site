fdd2e4872b466b6ff8dc9d881f154544a603f668
proud canadian <113315327+downloadablecontent@users.noreply.github.com>
2023-07-25 10:10:04 -0700

    pull posts from db before build

--- mongo_fetch.js
+++ mongo_fetch.js
@@ -4,19 +4,23 @@
 import { MongoClient } from 'mongodb';
 import { writeFileSync } from 'fs';
 
+//uncomment if running locally, don't forget to `npm i dotenv` and put the mongodb stuff in the `.env` file
+//import { config } from 'dotenv';
+//config();
+
 //see if mongodb db connection string is set in env (make sure it is an encrypted secret if deploying with github pages)
 //the mongo db user should have READ ONLY permissions, so even if connection string is leaked, no one can fuck with the db
 if (process.env.MONGODB_CONNECTION_STRING) {
     //connect to mongodb
     const client = new MongoClient(process.env.MONGODB_CONNECTION_STRING);
-    async function main() {
-        await client.connect();
-        const db = client.db("TongMemorialDatabase");
-        const posts_coll = db.collection("blog_posts");
-        //fetch all blog posts
-        let all_posts = await (await posts_coll.find({ hidden: false })).toArray();
-        //write to json file
-        //writeFileSync("./src/lib/blog_posts.json", JSON.stringify(all_posts), "utf-8");
-    }
-    main();
+    await client.connect();
+    const db = client.db("TongMemorialDatabase");
+    const posts_coll = db.collection("blog_posts");
+    //fetch all blog posts
+    let all_posts = await (await posts_coll.find({ hidden: false })).toArray();
+    all_posts.forEach((item) => delete item._id)
+    //write to json file
+    writeFileSync("./src/lib/blog_posts.json", JSON.stringify(all_posts), "utf-8");
+    console.log('done')
+    process.exit();
 }
--- src/routes/blog/+page.server.ts
+++ src/routes/blog/+page.server.ts
@@ -1,10 +1,11 @@
 import dummy_posts from '$lib/dummy_posts.json';
+import blog_posts from '$lib/blog_posts.json';
 
 /* fetch blog posts? */
 
 export async function load() {
     //return fake posts
     return {
-        posts: dummy_posts,
+        posts: blog_posts.length !== 0 ? blog_posts : dummy_posts,
     }
 }
